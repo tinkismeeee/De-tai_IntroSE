aebb29d9cf26d2049b687338f453616eb61fc20c
tinkismeeee <tinhisme1@gmail.com>
2025-06-03 01:32:06 +0700

    Update 03/06

---
+++ DO_AN/Controllers/BuyController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DO_AN.Controllers
+{
+    public class BuyController : Controller
+    {
+        public IActionResult iphone16_128gb()
+        {
+            return View();
+        }
+    }
+}

cffcb9d319c582456fed1057054a2888d198a601
thaikhang113 <cvcaothaikhang29@gmail.com>
2025-06-03 02:57:11 +0700

    upload

---
+++ DO_AN/Models/Accessory.cs
@@ -0,0 +1,15 @@
+﻿namespace DO_AN.Models
+{
+    public class Accessory
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Brand { get; set; }
+        public string Type { get; set; }
+        public int Price {  get; set; }
+        public string Description { get; set; }
+        public int StockQuantity { get; set; }
+        public string ImageUrl { get; set; }
+
+    }
+}

db8f8c10b1bbe08e78e6152dc91538b2a23bf69b
tinkismeeee <tinhisme1@gmail.com>
2025-06-05 07:09:20 +0700

    update 5/6

--- DO_AN/wwwroot/js/Buy.js
+++ DO_AN/wwwroot/js/Buy.js
@@ -6,3 +6,16 @@ function openPopup(){
 function closePopup(){
   popup.classList.remove('open-popup')
 }
+
+var swiper = new Swiper('.swiper-container', {
+  loop: true, // Lặp vô tận
+  spaceBetween: 10, // Khoảng cách giữa các slide
+  navigation: {
+  nextEl: '.swiper-button-next',
+  prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+  el: '.swiper-pagination',
+  clickable: true,
+  },
+});
